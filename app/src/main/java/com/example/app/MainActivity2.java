ba687b36ab3cecc25abb5455779f7df0b742b3ba
cheecho-huang <924972284@qq.com>
2020-07-12 17:05:04 +0800

    hwy

--- app/src/main/java/com/example/app/MainActivity2.java
+++ app/src/main/java/com/example/app/MainActivity2.java
@@ -1,14 +1,204 @@
 package com.example.app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity2 extends BaseActivity implements AdapterView.OnItemClickListener {
+
+    private NoteDatabase dbHelper;
+
+    private Context context = this;
+    private static final String TAG = "tag";
+    FloatingActionButton btn;
+    private EditText et;
+    private ListView lv;
+    private NoteAdapter adapter;
+    private List<Note> noteList = new ArrayList<Note>();
+    private Toolbar myToolbar;
+
+    public MainActivity2() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        btn = (FloatingActionButton)findViewById(R.id.fab);
+        et = (EditText)findViewById(R.id.et);
+        lv = (ListView)findViewById(R.id.lv);
+        myToolbar = findViewById(R.id.myToolbar);
+        adapter = new NoteAdapter(getApplicationContext(), noteList);
+        lv.setAdapter(adapter);
+        refreshListView();
+
+        //设置自定义状态栏
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //设置toolbar取代actionbar,自定义工具栏代替本来的活动栏
+
+        myToolbar.setNavigationIcon(R.drawable.ic_menu_dehaze_24);
+
+        lv.setOnItemClickListener(this);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity2.this,EditActivity.class);
+                intent.putExtra("mode",4);//新建笔记mode==4
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+    
+    //接收startActivityForResult结果判断增删改查
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        int returnMode;
+        long note_Id;
+        returnMode = data.getExtras().getInt("mode", -1);
+        note_Id = data.getExtras().getLong("id", 0);
+
+
+        if (returnMode == 1) {  //update current note
+
+            String content = data.getExtras().getString("content");
+            String time = data.getExtras().getString("time");
+            int tag = data.getExtras().getInt("tag", 1);
+
+            Note newNote = new Note(content, time, tag);
+            newNote.setId(note_Id);// ID不变
+            CRUD op = new CRUD(context);
+            op.open();
+            op.updateNote(newNote);
+            op.close();
+        } else if (returnMode == 0) {  // create new note
+            String content = data.getExtras().getString("content");
+            String time = data.getExtras().getString("time");
+            int tag = data.getExtras().getInt("tag", 1);
+
+            Note newNote = new Note(content, time, tag);
+            CRUD op = new CRUD(context);
+            op.open();
+            op.addNote(newNote);
+            op.close();
+        }else if(returnMode == 2){//delete
+            Note curNote = new Note();
+            curNote.setId(note_Id);
+            CRUD op = new CRUD(context);
+            op.open();
+            op.removeNote(curNote);
+            op.close();
+        }
+        else{}
+        refreshListView();
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        //search setting
+        MenuItem mSearch = menu.findItem(R.id.action_search);
+        SearchView mSearchView = (SearchView) mSearch.getActionView();//搜索界面
+
+        mSearchView.setQueryHint("search");
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            //随newText变化显示内容
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //清空数据库数据
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menu_clear:
+                new AlertDialog.Builder(MainActivity2.this)
+                        .setMessage("是否删除全部？")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dbHelper = new NoteDatabase(context);
+                                SQLiteDatabase db = dbHelper.getWritableDatabase();//writable指可读可写
+                                db.delete("notes",null,null);//清空数据库数据
+                                db.execSQL("update sqlite_sequence set seq=0 where name='notes'");//把数据库ID重新设置为0
+                                refreshListView();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .create().show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    //刷新列表
+    public void refreshListView(){
+
+        CRUD op = new CRUD(context);
+        op.open();
+        // set adapter
+        if (noteList.size() > 0) noteList.clear();
+        noteList.addAll(op.getAllNotes());
+        op.close();
+        adapter.notifyDataSetChanged();
+    }
+
+    //edit note
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()) {
+            case R.id.lv:
+                Note curNote = (Note) parent.getItemAtPosition(position);
+                Intent intent = new Intent(MainActivity2.this, EditActivity.class);
+                intent.putExtra("content", curNote.getContent());
+                intent.putExtra("id", curNote.getId());
+                intent.putExtra("time", curNote.getTime());
+                intent.putExtra("mode", 3);     // 修改笔记mode==3
+                intent.putExtra("tag", curNote.getTag());
+                startActivityForResult(intent, 1);      //collect data from edit
+                Log.d(TAG, "onItemClick: " + position);
+                break;
+        }
     }
 }
